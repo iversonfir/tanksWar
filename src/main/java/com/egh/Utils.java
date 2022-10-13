4e211e1758ae3d749597a138213a8340c2220c41
iversonfir <iversonfir@gmail.com>
2022-10-13 11:35:34 +0800

    [TODO] playAudio()

--- src/main/java/com/egh/Utils.java
+++ src/main/java/com/egh/Utils.java
@@ -10,4 +10,13 @@ public class Utils
     {
         return new ImageIcon("assets/images/" + imageName).getImage();
     }
+
+    //TODO playAudio()
+//    public static void playAudio(String fileName)
+//    {
+//        Media sound = new Media(new File("assets/audios/" + fileName).toURI().toString());
+//        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+//        mediaPlayer.play();
+//    }
 }
+
